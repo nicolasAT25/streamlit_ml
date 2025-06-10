e9037e277d05272531526dc741d8571c4c21ad37
Nicolás Aranguren Turmequé <nicolasarangurenturmeque@Laptop-de-Nicolas.local>
2025-06-10 08:42:16 -0500

    Initial commit

--- src/ml_model_streamlit.py
+++ src/ml_model_streamlit.py
@@ -8,12 +8,7 @@ import numpy as np
 import plotly.express as px
 
 #------------------------------------------------------------------------------------------------
-from feature_engine.imputation import CategoricalImputer, MeanMedianImputer, AddMissingIndicator
-from feature_engine.encoding import OrdinalEncoder, RareLabelEncoder
-from sklearn.preprocessing import MinMaxScaler, Binarizer
-from sklearn.pipeline import Pipeline
 
-import input.preprocessors as pp
 from configurations import config
 
 #------------------------------------------------------------------------------------------------
